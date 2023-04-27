4ff27a3f79167a17337470134c3de2551d88aba5
johe01 <104206851+johe01@users.noreply.github.com>
2023-04-27 17:49:34 +0900

    Add files via upload

---
+++ QRActivity.java
@@ -0,0 +1,30 @@
+package com.example.camerapreview1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QRActivity extends AppCompatActivity {
+
+    Button btnRtn;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+
+        btnRtn = findViewById(R.id.btnReturn);
+
+        btnRtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+
+}
